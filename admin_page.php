40f31798271e873af8df4962768623c08c09c536
Zeenat Mustafa <chzeenatmustafa2310@gmail.com>
2025-05-16 11:20:25 +0500

    Fix: Add style.css path link in user and admin page

--- admin_page.php
+++ admin_page.php
@@ -14,6 +14,7 @@ if(!isset($_SESSION['email'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Page</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body style="background: #fff;">
 
--- user_page.php
+++ user_page.php
@@ -14,6 +14,7 @@ if(!isset($_SESSION['email'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>User Page</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body style="background: #fff;">
 
