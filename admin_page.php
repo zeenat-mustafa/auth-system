fd7c2ea9ac9789a866cc2260615bac9b60e090be
Zeenat Mustafa <chzeenatmustafa2310@gmail.com>
2025-05-16 11:01:31 +0500

    Add structure for admin page

---
+++ admin_page.php
@@ -0,0 +1,27 @@
+<?php
+
+session_start();
+if(!isset($_SESSION['email'])) {
+    header("Location: index.php");
+    exit();
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Page</title>
+</head>
+<body style="background: #fff;">
+
+    <div class="box">
+        <h1>Welcome, <span><?= $_SESSION['name']; ?></span></h1>
+        <p>This is an <span>admin</span> page</p>
+        <button onclick="window.location.href='logout.php'">Logout</button>
+    </div>
+    
+</body>
+</html>
